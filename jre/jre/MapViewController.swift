db9849c16e15c5444b5fedee173071978a4027cc
Joe Van Gundy <vangundy.22@osu.edu>
2016-02-22 14:06:45 -0500

    Works for local device now

---
+++ jre/jre/MapViewController.swift
@@ -0,0 +1,101 @@
+//
+//  MapViewController.swift
+//  jre
+//
+//  Created by Joey Van Gundy on 2/19/16.
+//  Copyright © 2016 Joe Van Gundy. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    let locationManager = CLLocationManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+        
+        //Decide the zoom and position of default path
+        //let camera = GMSCameraPosition.cameraWithLatitude(-33.86,longitude: 151.20, zoom: 6)
+        //let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        //mapView.myLocationEnabled = true
+        //self.view = mapView
+
+        
+        
+//        let markerPosition = CLLocationCoordinate2DMake(-33.86, 151.20)
+//        let markerTitle = "Yooooo"
+//        
+//    
+//        //Return a marker that needs to be added to the map
+//        let marker = createMarker(markerPosition, markerTitle: markerTitle)
+//        marker.map = mapView
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func createMarker(markerPosition: CLLocationCoordinate2D, markerTitle: String) ->GMSMarker{
+        let marker = GMSMarker()
+        marker.title = markerTitle
+        marker.position = markerPosition
+        return marker
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: - CLLocationManagerDelegate
+//1
+extension MapViewController: CLLocationManagerDelegate {
+    // 2
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // 3
+        if status == .AuthorizedWhenInUse {
+            
+            // 4
+            locationManager.startUpdatingLocation()
+            
+            //5
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
+    
+    // 6
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            
+            // 7
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            
+            // 8
+            locationManager.stopUpdatingLocation()
+        }
+        
+    }
+}
